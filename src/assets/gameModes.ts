e064f7dc9762e8de7158c33efbc681d4e5ed2040
bruno <brunoqueiroz612@gmail.com>
2023-07-18 08:28:43 -0300

    Create gameModes data

---
+++ src/assets/gameModes.ts
@@ -0,0 +1,19 @@
+export const gameModes = [
+  {
+    mode: "Traditional",
+    description: "Practice typing with random texts of books, movies etc...",
+    slug: "traditional",
+  },
+  {
+    mode: "Repeated Words",
+    description:
+      "Get better at writting specific words, repetition is the key.",
+    slug: "repeated-words",
+  },
+  {
+    mode: "Code",
+    description:
+      "This mode help you to code faster, you could even be your own prettier.",
+    slug: "code",
+  },
+];
