4c73d9e45ab186fe341a299e327047dad8ae8b3a
bruno <brunoqueiroz612@gmail.com>
2023-07-20 08:09:30 -0300

    Show if input is right or wrong

--- src/app/practice/[mode]/page.tsx
+++ src/app/practice/[mode]/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 import Link from "next/link";
+import { ChangeEvent, useRef, useState } from "react";
 
-const text = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus,
+const text = `dolor sit amet consectetur adipisicing elit. Minus,
 assumenda laborum earum eos autem architecto rerum eligendi
 similique nemo hic voluptatum in ut alias exercitationem quod, animi
 corrupti, vero unde!`;
@@ -13,19 +14,77 @@ for (let i = 0; i < text.length; i++) {
   textArray.push(char);
 }
 
+interface MissingTypes extends Event {
+  inputType: string;
+}
+
 const Page = ({ params }: { params: { mode: string } }) => {
+  const [input, setInput] = useState("");
+  const [isMisspelled, setIsMisspelled] = useState({
+    is: false,
+    index: 0,
+  });
+  const textElement = useRef<HTMLParagraphElement>(null);
+
+  const type = (e: ChangeEvent<HTMLInputElement>) => {
+    const isDeleteContentBackward =
+      (e.nativeEvent as MissingTypes).inputType === "deleteContentBackward";
+
+    if (textElement.current) {
+      const currentCharElement = textElement.current?.children[
+        input.length
+      ] as HTMLSpanElement;
+
+      setInput(e.target.value);
+      const currentText = e.target.value;
+      const currentLastChar = e.target.value[currentText.length - 1];
+
+      if (
+        textArray[input.length] === currentLastChar &&
+        !isMisspelled.is &&
+        !isDeleteContentBackward
+      ) {
+        currentCharElement.style.color = "green";
+      } else if (isDeleteContentBackward) {
+        const currentCharElement = textElement.current?.children[
+          input.length - 1
+        ] as HTMLSpanElement;
+        if (isMisspelled.is && isMisspelled.index === input.length - 1) {
+          setIsMisspelled({
+            is: false,
+            index: 0,
+          });
+        }
+
+        currentCharElement.style.color = "black";
+        currentCharElement.style.backgroundColor = "transparent";
+      } else {
+        if (!isMisspelled.is) {
+          setIsMisspelled({ is: true, index: input.length });
+        }
+        currentCharElement.style.backgroundColor = "#F87171";
+      }
+    }
+  };
+
   return (
     <section className="p-4">
       <div className="flex flex-col gap-4 bg-gray-200 rounded-md p-4">
         <div>practice progress</div>
         <div className="flex flex-col gap-4 p-4 bg-gray-300 rounded">
-          <p>
+          <p ref={textElement}>
             {textArray.map((char, index) => (
               <span key={index}>{char}</span>
             ))}
           </p>
 
-          <input type="text" spellCheck="false" className="p-2 bg-gray-200" />
+          <input
+            type="text"
+            spellCheck="false"
+            className="p-2 bg-gray-200"
+            value={input}
+            onChange={type}
+          />
         </div>
         <div className="flex justify-between">
           <Link
