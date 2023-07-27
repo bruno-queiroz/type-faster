0f2c365d073e17ccc6344e241a5bcf898ec7e2db
bruno <brunoqueiroz612@gmail.com>
2023-07-27 08:00:11 -0300

    Create checkWord utility function

---
+++ src/utils/checkWord.ts
@@ -0,0 +1,40 @@
+export const checkWord = (
+  inputIndex: number,
+  inputValue: string,
+  elements: HTMLCollection,
+  textArray: string[],
+  currentWordBeginningIndex: number
+) => {
+  let isMisspelled = {
+    is: false,
+    index: 0,
+  };
+
+  const isTheFirstWord = currentWordBeginningIndex === 0;
+
+  for (let i = 0; i < inputValue.length; i++) {
+    const inputChar = inputValue[i];
+    const textChar = isTheFirstWord
+      ? textArray[i]
+      : textArray[currentWordBeginningIndex + i];
+    const currentCharElement = isTheFirstWord
+      ? (elements[i] as HTMLSpanElement)
+      : (elements[currentWordBeginningIndex + i] as HTMLSpanElement);
+
+    if (inputChar === textChar && !isMisspelled.is) {
+      currentCharElement.style.color = "green";
+      currentCharElement.style.backgroundColor = "white";
+    } else {
+      if (!isMisspelled.is) {
+        isMisspelled = {
+          is: true,
+          index: isTheFirstWord ? i : currentWordBeginningIndex + i,
+        };
+      }
+      currentCharElement.style.backgroundColor = "blue";
+      currentCharElement.style.color = "black";
+    }
+  }
+
+  return isMisspelled;
+};
