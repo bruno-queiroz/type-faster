024ff3caf6a6e8935597aa3fd1c0de177f63f2ed
bruno <brunoqueiroz612@gmail.com>
2023-08-04 08:39:52 -0300

    Add cursor on the first element

--- src/utils/addCursor.ts
+++ src/utils/addCursor.ts
@@ -1,6 +1,19 @@
 export const addCursor = (inputIndex: number, elements: HTMLCollection) => {
   const currentCharElement = elements[inputIndex] as HTMLSpanElement;
   const nextElement = elements[inputIndex + 1] as HTMLSpanElement;
+  const firstCharElement = elements[0] as HTMLSpanElement;
+
+  const isFirstChar = inputIndex === -1;
+  if (isFirstChar) {
+    firstCharElement.style.position = "relative";
+
+    const cursor = document.createElement("div");
+    cursor.style.left = "0";
+    cursor.classList.add("cursor");
+
+    firstCharElement.appendChild(cursor);
+    return;
+  }
 
   if (currentCharElement) {
     if (currentCharElement.textContent === " " && nextElement) {
