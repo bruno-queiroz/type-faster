ca69bddf21bcff7e9e7cb0ae40c135f43aeee7e6
bruno <brunoqueiroz612@gmail.com>
2023-08-04 08:38:48 -0300

    Remove cursor from the first element

--- src/utils/removeCursor.ts
+++ src/utils/removeCursor.ts
@@ -1,6 +1,14 @@
 export const removeCursor = (inputIndex: number, elements: HTMLCollection) => {
   const spanElement = elements[inputIndex];
   const nextSpanElement = elements[inputIndex + 1];
+  const firstCharElement = elements[0];
+
+  const isFirstChar = inputIndex === -1;
+  if (isFirstChar) {
+    const cursor = firstCharElement?.querySelector("div");
+    cursor?.remove();
+    return;
+  }
 
   if (spanElement?.textContent === " ") {
     const cursor = nextSpanElement?.querySelector("div");
