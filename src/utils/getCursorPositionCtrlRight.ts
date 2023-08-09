adb2ca8123710a28580ece54800b57383c4b762c
bruno <brunoqueiroz612@gmail.com>
2023-08-09 08:48:38 -0300

    Create getCursorPositionCtrlRight utility function

---
+++ src/utils/getCursorPositionCtrlRight.ts
@@ -0,0 +1,27 @@
+export const getCursorPositionCtrlRight = (
+  input: string,
+  cursorIndex: number
+) => {
+  const isWordChar = /\w/;
+
+  if (!isWordChar.test(input[cursorIndex + 1])) {
+    return cursorIndex + 1;
+  }
+
+  if (!isWordChar.test(input[cursorIndex])) {
+    cursorIndex++;
+  }
+
+  while (
+    isWordChar.test(input[cursorIndex]) &&
+    cursorIndex < input.length - 1
+  ) {
+    cursorIndex++;
+  }
+
+  if (!isWordChar.test(input[cursorIndex])) {
+    cursorIndex--;
+  }
+
+  return cursorIndex;
+};
