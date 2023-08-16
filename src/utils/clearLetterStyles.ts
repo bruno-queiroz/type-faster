a3d6eae450cc665027e8d3296d3f8a57c17532b1
bruno <brunoqueiroz612@gmail.com>
2023-08-16 08:52:04 -0300

    Check if currentCharElement exist

--- src/utils/clearLetterStyles.ts
+++ src/utils/clearLetterStyles.ts
@@ -4,8 +4,9 @@ export const clearLetterStyles = (
   inputIndex: number
 ) => {
   for (let i = 0; i < letterAmount; i++) {
-    if (inputIndex - i - 1 > elements.length - 1) continue;
     const currentCharElement = elements[inputIndex - i - 1] as HTMLSpanElement;
+
+    if (!currentCharElement) continue;
 
     currentCharElement.style.color = "black";
     currentCharElement.style.backgroundColor = "transparent";
--- src/utils/removeCursor.ts
+++ src/utils/removeCursor.ts
@@ -1,6 +1,6 @@
 export const removeCursor = (inputIndex: number, elements: HTMLCollection) => {
-  const spanElement = elements[inputIndex] as HTMLSpanElement;
-  const nextSpanElement = elements[inputIndex + 1] as HTMLSpanElement;
+  const currentCharElement = elements[inputIndex] as HTMLSpanElement;
+  const nextCharElement = elements[inputIndex + 1] as HTMLSpanElement;
   const firstCharElement = elements[0] as HTMLSpanElement;
 
   const isFirstChar = inputIndex === -1;
@@ -9,9 +9,11 @@ export const removeCursor = (inputIndex: number, elements: HTMLCollection) => {
     return;
   }
 
-  if (spanElement?.textContent === " ") {
-    nextSpanElement.style.boxShadow = "";
+  if (!currentCharElement) return;
+
+  if (currentCharElement?.textContent === " ") {
+    nextCharElement.style.boxShadow = "";
   } else {
-    spanElement.style.boxShadow = "";
+    currentCharElement.style.boxShadow = "";
   }
 };
