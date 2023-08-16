6a702b78fdba346cff7f68254803735ad6c8e3c1
bruno <brunoqueiroz612@gmail.com>
2023-08-16 08:01:17 -0300

    Create getAccuracy utility function

---
+++ src/utils/getAccuracy.ts
@@ -0,0 +1,6 @@
+export const getAccuracy = (mistakeAmount: number, inputLength: number) => {
+  const mistakePercentage = (mistakeAmount * 100) / inputLength;
+  const accuracyPercentage = 100 - mistakePercentage;
+  const accuracyFormatted = accuracyPercentage.toFixed(2);
+  return accuracyFormatted;
+};
