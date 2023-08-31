289df9ba392aabd3090e24ad781cbef344947373
bruno <brunoqueiroz612@gmail.com>
2023-08-31 07:29:00 -0300

    Create Mistakes component

---
+++ src/components/Mistakes.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { wordsTypedWrong } from "@/app/practice/[mode]/page";
+
+const Mistakes = () => {
+  return (
+    <article>
+      <h2 className="text-xl my-4">Mistakes</h2>
+      <div className="bg-gray-200 p-4">
+        <div className="flex gap-2">
+          {[...wordsTypedWrong].map((word, i) => (
+            <span
+              key={i}
+              className="bg-white p-2 rounded w-[max-content] border-b-[1px] border-b-red-500"
+            >
+              {word}
+            </span>
+          ))}
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default Mistakes;
