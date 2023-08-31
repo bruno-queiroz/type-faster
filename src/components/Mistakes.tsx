da693bb2e779a65058568e633cc8ff3a713e1778
bruno <brunoqueiroz612@gmail.com>
2023-08-31 08:18:49 -0300

    When user hasn't made any mistakes, show a message

--- src/components/Mistakes.tsx
+++ src/components/Mistakes.tsx
@@ -8,14 +8,16 @@ const Mistakes = () => {
       <h2 className="text-xl my-4">Mistakes</h2>
       <div className="bg-gray-200 p-4">
         <div className="flex gap-2">
-          {[...wordsTypedWrong].map((word, i) => (
-            <span
-              key={i}
-              className="bg-white p-2 rounded w-[max-content] border-b-[1px] border-b-red-500"
-            >
-              {word}
-            </span>
-          ))}
+          {wordsTypedWrong.size === 0
+            ? "No mistakes this time! Good Job 🤠"
+            : [...wordsTypedWrong].map((word, i) => (
+                <span
+                  key={i}
+                  className="bg-white p-2 rounded w-[max-content] border-b-[1px] border-b-red-500"
+                >
+                  {word}
+                </span>
+              ))}
         </div>
       </div>
     </article>
