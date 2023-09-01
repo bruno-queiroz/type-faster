a0b4cd0e1177facc495b7b0b080e9b13d31953af
bruno <brunoqueiroz612@gmail.com>
2023-09-01 08:10:21 -0300

    Import typos

--- src/components/Mistakes.tsx
+++ src/components/Mistakes.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { wordsTypedWrong } from "@/app/practice/[mode]/page";
+import { typos } from "@/app/practice/[mode]/page";
 
 const Mistakes = () => {
   return (
@@ -8,9 +8,9 @@ const Mistakes = () => {
       <h2 className="text-xl my-4">Mistakes</h2>
       <div className="bg-gray-200 p-4">
         <div className="flex gap-2">
-          {wordsTypedWrong.size === 0
+          {typos.length === 0
             ? "No mistakes this time! Good Job 🤠"
-            : [...wordsTypedWrong].map((word, i) => (
+            : typos.map(({ word }, i) => (
                 <span
                   key={i}
                   className="bg-white p-2 rounded w-[max-content] border-b-[1px] border-b-red-500"
