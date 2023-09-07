2e7e58b6aba363532fe15bf49a771141980e8533
bruno <brunoqueiroz612@gmail.com>
2023-09-07 08:36:05 -0300

    Change getCPM to return a string

--- src/utils/getCPM.ts
+++ src/utils/getCPM.ts
@@ -6,6 +6,6 @@ export const getCPMContext = () => {
     const elapsedTimeInSeconds = elapsedTime / 1000;
     const cpm = (inputLength * 60) / elapsedTimeInSeconds;
 
-    return { cpm: Math.trunc(cpm), initialDate };
+    return Math.trunc(cpm).toString();
   };
 };
