a9cf41586ad550f074bdacc1d098d3a540a4690a
bruno <brunoqueiroz612@gmail.com>
2023-09-07 07:36:11 -0300

    Add cursor to type review

--- src/components/TypeReview.tsx
+++ src/components/TypeReview.tsx
@@ -7,18 +7,30 @@ import { MdReplay as ReplayIcon } from "react-icons/md";
 import { useTypeReview } from "@/hooks/useTypeReview";
 
 const TypeReview = () => {
-  const { handlePlayReview, isPlaying, restartTypingReview, typingReview } =
-    useTypeReview();
+  const {
+    handlePlayReview,
+    isPlaying,
+    restartTypingReview,
+    typingReview,
+    isCursorShowing,
+  } = useTypeReview();
 
   return (
     <article>
       <h2 className="text-xl my-4">Type Review</h2>
 
       <div className="flex flex-col gap-4 bg-gray-200 p-4">
-        <p className="w-full h-[100px] bg-gray-300 rounded p-2">
+        <p className="w-full h-[100px] flex-wrap bg-gray-300 rounded p-2">
           {typingReview.map((char, index) => (
             <span key={index}>{char.value}</span>
           ))}
+          <div className="inline-block relative w-[1px] h-[20px]">
+            <span
+              className={`absolute bottom-[-3px] inline-block w-[1px] h-[20px] ${
+                isCursorShowing && "cursor-on-right"
+              }`}
+            />
+          </div>
         </p>
         <div className="flex gap-4">
           <button
