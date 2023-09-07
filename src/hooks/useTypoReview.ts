abdab6740e16dca0cc1caa572dbdc6eb48954a88
bruno <brunoqueiroz612@gmail.com>
2023-09-07 07:58:09 -0300

    Create useTypoReview custom hook

---
+++ src/hooks/useTypoReview.ts
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+import { TypingReview, typingHistory, typos } from "@/app/practice/[mode]/page";
+
+import { clearAllSetIntervals } from "@/utils/clearAllSetIntervals";
+import { getEndOfWordIndex } from "@/utils/getEndOfWordIndex";
+import { getStartOfWordIndex } from "@/utils/getStartOfWordIndex";
+import { playTypingReview } from "@/utils/playTypingReview";
+
+export const useTypoReview = () => {
+  const [typoReview, setTypoReview] = useState<TypingReview[]>([]);
+  const [isCursorShowing, setIsCursorShowing] = useState(false);
+
+  const showTypoReplay = (index: number) => {
+    setTypoReview([]);
+    const wordInitialIndex = getStartOfWordIndex(
+      typos[index].typingHistoryIndex,
+      typingHistory
+    );
+
+    const wordFinalIndex = getEndOfWordIndex(
+      typos[index].typingHistoryIndex,
+      typingHistory
+    );
+
+    const replaySlice = typingHistory.slice(wordInitialIndex, wordFinalIndex);
+
+    setIsCursorShowing(true);
+
+    playTypingReview({
+      typingHistory: replaySlice,
+      reviewFinishedCallBack: () => setIsCursorShowing(false),
+      typingReviewIndex: 0,
+      setTypingReview: setTypoReview,
+    });
+  };
+
+  const clearReplay = () => {
+    clearAllSetIntervals(0);
+  };
+
+  return {
+    typoReview,
+    isCursorShowing,
+    clearReplay,
+    showTypoReplay,
+  };
+};
