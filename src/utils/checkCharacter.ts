f4b409c2bc5b13fe01c374d3490823ee324d77ab
bruno <brunoqueiroz612@gmail.com>
2023-09-14 08:49:02 -0300

    Refactor checkCharacter utility function

--- src/utils/checkCharacter.ts
+++ src/utils/checkCharacter.ts
@@ -1,34 +1,29 @@
-import { checkEllipsis } from "./checkEllipsis";
+export const WORD_CHARACTER_REGEX = /\w|'/;
 
 export const checkCharacter = (
   input: string,
   cursorIndex: number,
-  ctrl: "left" | "right",
+  checkEllipsisStrategy: (
+    input: string,
+    cursorIndex: number,
+    initialIndex?: number
+  ) => {
+    isEllipsis: boolean;
+    jumpCharAmount: number;
+  },
   initialIndex?: number
 ) => {
-  const regex = /\w|'/;
+  const { isEllipsis, jumpCharAmount } = checkEllipsisStrategy(
+    input,
+    cursorIndex,
+    initialIndex
+  );
 
-  let inputSlice = "";
-
-  if (ctrl === "left") {
-    inputSlice =
-      input[cursorIndex] + input[cursorIndex - 1] + input[cursorIndex - 2];
-  } else {
-    inputSlice =
-      input[cursorIndex] + input[cursorIndex + 1] + input[cursorIndex + 2];
-  }
-
-  const { isEllipsis, jumpCharAmount } = checkEllipsis(inputSlice);
-
-  if (input[initialIndex! + 1] === "." && ctrl === "right") {
-    if (isEllipsis) {
-      return { isChar: true, isEllipsis, jumpCharAmount };
-    }
-  } else if (ctrl === "left" && isEllipsis) {
+  if (isEllipsis) {
     return { isChar: true, isEllipsis, jumpCharAmount };
   }
 
-  const isChar = regex.test(input[cursorIndex]);
+  const isChar = WORD_CHARACTER_REGEX.test(input[cursorIndex]);
 
   return { isChar, isEllipsis: false, jumpCharAmount: 0 };
 };
