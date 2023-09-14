826784674257d77a0b534cf294c8753bb929d87e
bruno <brunoqueiroz612@gmail.com>
2023-09-14 08:50:47 -0300

    Refactor getCursorPositionCtrlLeft

--- src/utils/getCursorPositionCtrlLeft.ts
+++ src/utils/getCursorPositionCtrlLeft.ts
@@ -1,20 +1,25 @@
 import { checkCharacter } from "./checkCharacter";
+import { checkEllipsisCtrlLeft } from "./checkEllipsisCtrlLeft";
 
 export const getCursorPositionCtrlLeft = (
   input: string,
   cursorIndex: number
 ) => {
-  const isChar = checkCharacter(input, cursorIndex, "left").isChar;
+  const isChar = checkCharacter(
+    input,
+    cursorIndex,
+    checkEllipsisCtrlLeft
+  ).isChar;
 
   if (!isChar) {
     return cursorIndex - 1;
   }
 
   while (
-    checkCharacter(input, cursorIndex, "left").isChar &&
+    checkCharacter(input, cursorIndex, checkEllipsisCtrlLeft).isChar &&
     cursorIndex >= 0
   ) {
-    const character = checkCharacter(input, cursorIndex, "left");
+    const character = checkCharacter(input, cursorIndex, checkEllipsisCtrlLeft);
 
     if (character.isEllipsis) {
       cursorIndex -= character.jumpCharAmount;
