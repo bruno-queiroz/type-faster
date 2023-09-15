7218d105bf9273ef3f41261652e3caf590afd113
bruno <brunoqueiroz612@gmail.com>
2023-09-15 09:01:32 -0300

    Refactor getCursorPositionCtrlLeft utility function

--- src/utils/getCursorPositionCtrlLeft.ts
+++ src/utils/getCursorPositionCtrlLeft.ts
@@ -1,23 +1,21 @@
-import { WORD_CHARACTER_REGEX, checkCharacter } from "./checkCharacter";
+import { checkCharacter } from "./checkCharacter";
 import { checkEllipsisCtrlLeft } from "./checkEllipsisCtrlLeft";
 
 export const getCursorPositionCtrlLeft = (
   input: string,
   cursorIndex: number
 ) => {
-  const isCurrentItemWordChar = WORD_CHARACTER_REGEX.test(input[cursorIndex]);
+  const initialIndex = cursorIndex;
 
-  if (!isCurrentItemWordChar) {
-    return cursorIndex - 1;
-  }
+  while (cursorIndex >= 0) {
+    const character = checkCharacter(
+      input,
+      cursorIndex,
+      checkEllipsisCtrlLeft,
+      initialIndex
+    );
 
-  while (
-    checkCharacter(input, cursorIndex, checkEllipsisCtrlLeft).isChar &&
-    cursorIndex >= 0
-  ) {
-    const character = checkCharacter(input, cursorIndex, checkEllipsisCtrlLeft);
-
-    if (character.isEllipsis) {
+    if (!character.isWordChar) {
       cursorIndex -= character.jumpCharAmount;
       break;
     }
