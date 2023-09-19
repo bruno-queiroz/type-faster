e1045dea20eb308e10ed2f5dff6faace1fde077d
bruno <brunoqueiroz612@gmail.com>
2023-09-19 07:58:01 -0300

    Refactor onKeyDownChangeCursor utility function

--- src/utils/onKeyDownChangeCursor.ts
+++ src/utils/onKeyDownChangeCursor.ts
@@ -2,69 +2,83 @@ import { KeyboardEvent, RefObject } from "react";
 
 import { addCursor } from "@/utils/addCursor";
 import { removeCursor } from "@/utils/removeCursor";
+import { getCursorPositionCtrlRight } from "./getCursorPositionCtrlRight";
+import { getCursorPositionCtrlLeft } from "./getCursorPositionCtrlLeft";
 
 export const onKeyDownChangeCursor = (
   e: KeyboardEvent<HTMLInputElement>,
   textElement: RefObject<HTMLParagraphElement>,
   currentWordBeginningIndex: number
 ) => {
-  if (textElement.current) {
-    const key = e.key;
-    const isCtrl = e.ctrlKey;
-    const cursorStart = (e.target as HTMLInputElement).selectionStart || 0;
-    const textElementChildren = textElement.current.children;
-    const inputValue = (e.target as HTMLInputElement).value;
-    const blackBg = "rgba(0, 0, 0, 0)";
-    const blackText = "rgb(0, 0, 0)";
+  if (!textElement.current) return;
 
-    if ((isCtrl && key === "ArrowLeft") || key === "ArrowUp") {
-      removeCursor(
-        currentWordBeginningIndex + cursorStart - 1,
-        textElementChildren
-      );
+  const key = e.key;
+  const isCtrl = e.ctrlKey;
+  const cursorStart = (e.target as HTMLInputElement).selectionStart || 0;
+  const inputValue = (e.target as HTMLInputElement).value;
 
-      addCursor(currentWordBeginningIndex - 1, textElementChildren);
-    } else if ((isCtrl && key === "ArrowRight") || key === "ArrowDown") {
-      removeCursor(
-        currentWordBeginningIndex + cursorStart - 1,
-        textElementChildren
-      );
+  const textElementChildren = textElement.current.children;
 
-      addCursor(
-        currentWordBeginningIndex + inputValue.length - 1,
-        textElementChildren
-      );
-    } else if (key === "ArrowLeft") {
-      const safeCursorStart = cursorStart - 2 < -1 ? -1 : cursorStart - 2;
+  if (key === "ArrowUp") {
+    removeCursor(
+      currentWordBeginningIndex + cursorStart - 1,
+      textElementChildren
+    );
 
-      removeCursor(
-        currentWordBeginningIndex + cursorStart - 1,
-        textElementChildren
-      );
+    addCursor(currentWordBeginningIndex - 1, textElementChildren);
+  } else if (key === "ArrowDown") {
+    removeCursor(
+      currentWordBeginningIndex + cursorStart - 1,
+      textElementChildren
+    );
 
-      addCursor(
-        currentWordBeginningIndex + safeCursorStart,
-        textElementChildren
-      );
-    } else if (key === "ArrowRight") {
-      if (textElementChildren[currentWordBeginningIndex + cursorStart]) {
-        const elementComputedStyles = getComputedStyle(
-          textElementChildren[currentWordBeginningIndex + cursorStart]
-        );
-        const elementColor = elementComputedStyles.color;
-        const elementBgColor = elementComputedStyles.backgroundColor;
+    addCursor(
+      currentWordBeginningIndex + inputValue.length - 1,
+      textElementChildren
+    );
+  } else if (isCtrl && key === "ArrowRight") {
+    removeCursor(
+      currentWordBeginningIndex + cursorStart - 1,
+      textElementChildren
+    );
 
-        if (elementColor === blackText && elementBgColor === blackBg) {
-          return;
-        }
+    const cursorPosition = getCursorPositionCtrlRight(
+      inputValue,
+      cursorStart - 1
+    );
 
-        removeCursor(
-          currentWordBeginningIndex + cursorStart - 1,
-          textElementChildren
-        );
+    addCursor(currentWordBeginningIndex + cursorPosition, textElementChildren);
+  } else if (isCtrl && key === "ArrowLeft") {
+    removeCursor(
+      currentWordBeginningIndex + cursorStart - 1,
+      textElementChildren
+    );
 
-        addCursor(currentWordBeginningIndex + cursorStart, textElementChildren);
-      }
-    }
+    const cursorPosition = getCursorPositionCtrlLeft(
+      inputValue,
+      cursorStart - 1
+    );
+
+    addCursor(currentWordBeginningIndex + cursorPosition, textElementChildren);
+  } else if (key === "ArrowLeft") {
+    const safeCursorStart = cursorStart - 2 < -1 ? -1 : cursorStart - 2;
+
+    removeCursor(
+      currentWordBeginningIndex + cursorStart - 1,
+      textElementChildren
+    );
+
+    addCursor(currentWordBeginningIndex + safeCursorStart, textElementChildren);
+  } else if (key === "ArrowRight") {
+    if (!textElementChildren[currentWordBeginningIndex + cursorStart]) return;
+
+    if (cursorStart >= inputValue.length) return;
+
+    removeCursor(
+      currentWordBeginningIndex + cursorStart - 1,
+      textElementChildren
+    );
+
+    addCursor(currentWordBeginningIndex + cursorStart, textElementChildren);
   }
 };
