685b95f25135c384ec46a2334c389977406b156d
bruno <brunoqueiroz612@gmail.com>
2023-09-27 08:57:50 -0300

    Skip letters if it is selected

--- src/utils/onKeyDownChangeCursor.ts
+++ src/utils/onKeyDownChangeCursor.ts
@@ -16,10 +16,12 @@ export const onKeyDownChangeCursor = (
 
   const key = e.key;
   const isCtrl = e.ctrlKey;
+  const isShift = e.shiftKey;
   const inputValue = (e.target as HTMLInputElement).value;
 
   const selectionStart = (e.target as HTMLInputElement).selectionStart || 0;
-  const selectionEnd = (e.target as HTMLInputElement).selectionEnd;
+  const selectionEnd = (e.target as HTMLInputElement).selectionEnd || 0;
+  const selection = document.getSelection()?.toString();
 
   const textElementChildren = textElement.current.children;
 
@@ -35,6 +37,13 @@ export const onKeyDownChangeCursor = (
       cursorIndex = inputValue.length - 1;
       break;
     case "ArrowRight":
+      if (isShift) {
+        removeCursor(
+          currentWordBeginningIndex + selectionStart - 1,
+          textElementChildren
+        );
+        break;
+      }
       if (isCtrl) {
         cursorIndex = getCursorPositionCtrlRight(
           inputValue,
@@ -47,11 +56,27 @@ export const onKeyDownChangeCursor = (
 
       if (selectionStart >= inputValue.length) break;
 
+      if (selection) {
+        cursorIndex = selectionEnd - 1;
+        break;
+      }
+
       cursorIndex = selectionStart;
       break;
     case "ArrowLeft":
+      if (isShift) {
+        removeCursor(
+          currentWordBeginningIndex + selectionStart - 1,
+          textElementChildren
+        );
+        break;
+      }
       if (isCtrl) {
         cursorIndex = getCursorPositionCtrlLeft(inputValue, selectionStart - 1);
+        break;
+      }
+      if (selection) {
+        cursorIndex = selectionStart - 1;
         break;
       }
       const safeCursorStart = selectionStart - 2 < -1 ? -1 : selectionStart - 2;
