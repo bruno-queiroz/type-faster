e8023d1781bc24293921ee0b2f196a04eddf9de8
bruno <brunoqueiroz612@gmail.com>
2023-09-28 08:50:39 -0300

    Add Title component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { gameModes } from "@/assets/gameModes";
 import GameModeCard from "@/components/GameModeCard";
+import Title from "@/components/Title";
 
 export default function Home() {
   return (
     <main className="p-4 ">
       <div className="flex justify-center">
         <div className="flex flex-col items-center">
-          <h1 className="mt-4 text-4xl font-bold">TypeFaster </h1>
+          <h1>
+            <Title>TypeFaster</Title>
+          </h1>
           <p>Get better at typing while having fun</p>
         </div>
       </div>
