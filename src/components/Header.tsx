f27870ce6fe736c1b829184f21e26ea05189cf87
bruno <brunoqueiroz612@gmail.com>
2023-09-28 16:18:25 -0300

    Change Header bg color to black

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="p-6 bg-red-400">
+    <header className="p-6 bg-neutral-900 mb-4 rounded-b-lg">
       <div>
-        <Link href={"/"} className="font-semibold">
+        <Link href={"/"} className="text-white font-semibold">
           TypeFaster
         </Link>
       </div>
