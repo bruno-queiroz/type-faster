5a3a1ece0ce618b3f240372cccec4923a91ef86f
bruno <brunoqueiroz612@gmail.com>
2023-10-06 08:24:02 -0300

    Create createText service api

---
+++ src/services/api/createText.ts
@@ -0,0 +1,20 @@
+interface Text {
+  text: string;
+  title: string;
+  author: string;
+  image: string;
+}
+
+export const createText = async (text: Text) => {
+  const response = await fetch("http://localhost:3333/api/text", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(text),
+  });
+
+  const data = await response.json();
+
+  return data;
+};
