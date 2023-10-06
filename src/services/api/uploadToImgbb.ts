7ec49bb703f2d3a5c786e021c7c8472e19f7a971
bruno <brunoqueiroz612@gmail.com>
2023-10-06 08:24:24 -0300

    Create uploadToImgbb service

---
+++ src/services/api/uploadToImgbb.ts
@@ -0,0 +1,53 @@
+export interface ImageData {
+  id: string;
+  title: string;
+  url_viewer: string;
+  url: string;
+  display_url: string;
+  width: string;
+  height: string;
+  size: string;
+  time: string;
+  expiration: string;
+  image: {
+    filename: string;
+    name: string;
+    mime: string;
+    extension: string;
+    url: string;
+  };
+  thumb: {
+    filename: string;
+    name: string;
+    mime: string;
+    extension: string;
+    url: string;
+  };
+  medium: {
+    filename: string;
+    name: string;
+    mime: string;
+    extension: string;
+    url: string;
+  };
+  delete_url: string;
+}
+
+export interface ImageResponse {
+  data: ImageData;
+  success: boolean;
+  status: number;
+}
+
+export const uploadToImgbb = async (text: FormData) => {
+  const response = await fetch(
+    `https://api.imgbb.com/1/upload?key=${process.env.NEXT_PUBLIC_IMGBB_API_KEY}`,
+    {
+      method: "POST",
+      body: text,
+    }
+  );
+  const data: ImageResponse = await response.json();
+  console.log(data);
+  return data;
+};
