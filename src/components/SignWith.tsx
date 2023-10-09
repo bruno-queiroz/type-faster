a772f880c21187a84bc218006d8c49731166ddd6
bruno <brunoqueiroz612@gmail.com>
2023-10-09 07:28:45 -0300

    Add button attributes to SignWith component

--- src/components/SignWith.tsx
+++ src/components/SignWith.tsx
@@ -1,13 +1,16 @@
-import React, { ReactNode } from "react";
+import React, { ButtonHTMLAttributes, ReactNode } from "react";
 
-interface SignWithProps {
+type SignWithProps = {
   children: ReactNode;
   labelText: string;
-}
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
-const SignWith = ({ children, labelText }: SignWithProps) => {
+const SignWith = ({ children, labelText, ...rest }: SignWithProps) => {
   return (
-    <button className="flex justify-center items-center gap-1 py-1 px-4 rounded border-[2px] border-neutral-700 text-neutral-700">
+    <button
+      className="flex justify-center items-center gap-1 py-1 px-4 rounded border-[2px] border-neutral-700 text-neutral-700"
+      {...rest}
+    >
       {children}
       <span>{labelText}</span>
     </button>
