3c33f929d8e9f7ae81ff70398d9e9cebf8aea667
bruno <brunoqueiroz612@gmail.com>
2023-10-12 16:12:54 -0300

    Add UserButton

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import React from "react";
+import UserButton from "./UserButton";
 
 const Header = () => {
   return (
     <header className="p-6 bg-neutral-900 mb-4 rounded-b-lg">
-      <div>
+      <div className="flex items-center justify-between">
         <Link href={"/"} className="text-white font-semibold">
           TypeFaster
         </Link>
+        <UserButton />
       </div>
     </header>
   );
