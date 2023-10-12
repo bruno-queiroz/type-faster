8b505d6a5514b7b6142a5fbc6cedff7b01e77032
bruno <brunoqueiroz612@gmail.com>
2023-10-12 16:12:40 -0300

    Create UserButton component

---
+++ src/components/UserButton.tsx
@@ -0,0 +1,22 @@
+import { StaticImageData } from "next/image";
+import UserPicture from "./UserPicture";
+import UserMenu from "./UserMenu";
+import UserMenuHeader from "./UserMenuHeader";
+
+export interface User {
+  name: string;
+  image: StaticImageData | string;
+}
+
+const UserButton = () => {
+  return (
+    <div className="w-[45px] h-[45px] rounded-full bg-green-500 relative">
+      <UserPicture />
+      <UserMenu>
+        <UserMenuHeader />
+      </UserMenu>
+    </div>
+  );
+};
+
+export default UserButton;
