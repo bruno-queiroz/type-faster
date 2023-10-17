61f758bc4a456596ebb3fb244e6076a46bd4457e
bruno <brunoqueiroz612@gmail.com>
2023-10-17 09:00:58 -0300

    Add types to getText fetch function

--- src/services/api/getText.ts
+++ src/services/api/getText.ts
@@ -1,6 +1,16 @@
+import { ServerDefaultResponse } from "./config";
+
+interface Text {
+  author: string;
+  id: string;
+  image: string;
+  text: string;
+  title: string;
+}
+
 export const getText = async () => {
   const response = await fetch("http://localhost:3333/api/text");
-  const data = response.json();
+  const data: ServerDefaultResponse<Text> = await response.json();
 
   return data;
 };
