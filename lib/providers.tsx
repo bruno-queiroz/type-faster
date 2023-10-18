cd52a0ab08de9e14244c3609a57f30910ddbdc22
bruno <brunoqueiroz612@gmail.com>
2023-10-18 08:57:22 -0300

    Disable some react query default options

--- lib/providers.tsx
+++ lib/providers.tsx
@@ -3,7 +3,14 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import React, { ReactNode } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
-  const [queryClient] = React.useState(() => new QueryClient());
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: { refetchOnWindowFocus: false, refetchOnMount: false },
+        },
+      })
+  );
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
