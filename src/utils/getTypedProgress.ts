dd64fde9bc0b73ba4abedaafb3fcc55a5386da30
bruno <brunoqueiroz612@gmail.com>
2023-10-18 07:32:12 -0300

    Return zero if textLength is not a truthy value

--- src/utils/getTypedProgress.ts
+++ src/utils/getTypedProgress.ts
@@ -1,4 +1,8 @@
-export const getTypedProgress = (textLength: number, lettersTyped: number) => {
+export const getTypedProgress = (
+  textLength: number | undefined,
+  lettersTyped: number
+) => {
+  if (!textLength) return 0;
   const progress = (lettersTyped * 100) / textLength;
 
   return progress;
