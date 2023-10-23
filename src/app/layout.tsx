3979bb24eabdb55e54b915e493b93d9905db8f02
bruno <brunoqueiroz612@gmail.com>
2023-10-23 15:39:06 -0300

    Add NextAuthProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import Header from "@/components/Header";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Providers from "../../lib/providers";
+import ReactQueryProvider from "../../lib/ReactQueryProvider";
+import { NextAuthProvider } from "../../lib/SessionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,10 +20,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
-          <Header />
-          {children}
-        </Providers>
+        <NextAuthProvider>
+          <ReactQueryProvider>
+            <Header />
+            {children}
+          </ReactQueryProvider>
+        </NextAuthProvider>
       </body>
     </html>
   );
