25e02b269cc7babe857c455a002134cd8be70e08
bruno <brunoqueiroz612@gmail.com>
2023-10-24 08:05:51 -0300

    Change UserMenu if user is logged or not

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -1,12 +1,18 @@
 "use client";
 import Link from "next/link";
 import { ReactNode, useState } from "react";
+import { signOut, useSession } from "next-auth/react";
 
 const UserMenu = ({ children }: { children: ReactNode }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { status } = useSession();
 
   const handleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleSignOut = () => {
+    signOut();
   };
   return (
     <div>
@@ -16,25 +22,43 @@ const UserMenu = ({ children }: { children: ReactNode }) => {
       />
 
       <div
-        className="w-full min-w-[max-content] absolute right-0 top-[55px] bg-neutral-800 rounded text-white pb-4"
+        className="w-full min-w-[180px] absolute right-0 top-[55px] bg-neutral-800 rounded text-white pb-4"
         style={{ display: isMenuOpen ? "block" : "none" }}
       >
         {children}
         <div className="border-t-[1px] border-t-neutral-500 w-[80%] mx-auto my-4" />
         <ul>
-          <li className="flex">
-            <Link
-              href={"/progress"}
-              className="hover:bg-neutral-700 px-4 py-2 w-full text-center"
-            >
-              Progress
-            </Link>
-          </li>
-          <li className="flex">
-            <button className="hover:bg-neutral-700 px-4 py-2 w-full text-center">
-              Sign Out
-            </button>
-          </li>
+          {status === "authenticated" ? (
+            <>
+              <li className="flex">
+                <Link
+                  href={"/progress"}
+                  className="hover:bg-neutral-700 px-4 py-2 w-full text-center"
+                >
+                  Progress
+                </Link>
+              </li>
+              <li className="flex">
+                <button
+                  className="hover:bg-neutral-700 px-4 py-2 w-full text-center"
+                  onClick={handleSignOut}
+                >
+                  Sign Out
+                </button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="flex">
+                <Link
+                  href={"/sign-up"}
+                  className="hover:bg-neutral-700 px-4 py-2 w-full text-center"
+                >
+                  Sign up
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </div>
