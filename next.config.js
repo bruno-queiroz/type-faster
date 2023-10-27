e402bb425664f0d0be82e7ff6eef004ba4c2a366
bruno <brunoqueiroz612@gmail.com>
2023-10-27 16:01:24 -0300

    Add googleusercontent domain to images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com", "i.ibb.co"],
+    domains: [
+      "avatars.githubusercontent.com",
+      "i.ibb.co",
+      "lh3.googleusercontent.com",
+    ],
   },
 };
 
