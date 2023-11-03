7ce73e244e0448d2f706d15abf41cf7a6319bd01
bruno <brunoqueiroz612@gmail.com>
2023-11-03 15:52:56 -0300

    Create getProgress fetch function

---
+++ src/services/api/getProgress.ts
@@ -0,0 +1,16 @@
+import { ServerDefaultResponse } from "./config";
+
+export interface Progress {
+  overallAverageCpm: number;
+  progress: {
+    createdAt: string;
+    cpm: number;
+  }[];
+}
+
+export const getProgress = async (email: string) => {
+  const response = await fetch(`http://localhost:3333/api/progress/${email}`);
+  const data: ServerDefaultResponse<Progress> = await response.json();
+
+  return data;
+};
