21709014274eb577317d9df6a91d378402179212
bruno <brunoqueiroz612@gmail.com>
2023-11-06 07:32:43 -0300

    Add 'no-cache' to getRanking fetch function

--- src/services/api/getRanking.ts
+++ src/services/api/getRanking.ts
@@ -2,14 +2,16 @@ import { ServerDefaultResponse } from "./config";
 
 interface Ranking {
   cpm: number;
-  createAt: string;
+  createdAt: string;
   user: {
     name: string;
   };
 }
 
 export const getRanking = async (textId: string) => {
-  const response = await fetch(`http://localhost:3333/api/ranking/${textId}`);
+  const response = await fetch(`http://localhost:3333/api/ranking/${textId}`, {
+    cache: "no-cache",
+  });
   const data: ServerDefaultResponse<Ranking[]> = await response.json();
 
   return data;
