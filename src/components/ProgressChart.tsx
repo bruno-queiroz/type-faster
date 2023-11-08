9788f2b94c031d5157983519d4c1a1f679bc9eaa
bruno <brunoqueiroz612@gmail.com>
2023-11-08 08:02:51 -0300

    Create ProgressChart component

---
+++ src/components/ProgressChart.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { Progress } from "@/services/api/getProgress";
+
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const ProgressChart = ({ progress }: Progress) => {
+  return (
+    <div className="p-4 rounded w-full aspect-video bg-gray-100">
+      <ResponsiveContainer>
+        <LineChart
+          data={progress}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 10,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="createdAt" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="cpm"
+            stroke="#16A34A"
+            activeDot={{ r: 8 }}
+          />
+          <Line
+            type="monotone"
+            dataKey="typos"
+            stroke="#EF4031"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default ProgressChart;
