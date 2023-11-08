2cf5bdf65346f7070b56358cd8cd85525e22bb2a
bruno <brunoqueiroz612@gmail.com>
2023-11-08 08:08:50 -0300

    Resize header size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,12 @@ import UserButton from "./UserButton";
 
 const Header = () => {
   return (
-    <header className="p-6 bg-neutral-900 mb-4 rounded-b-lg">
-      <div className="flex items-center justify-between">
-        <Link href={"/"} className="text-white font-semibold">
+    <header className="flex justify-center p-6 bg-neutral-900 mb-4 rounded-b-lg">
+      <div className="flex w-[85%] max-sm:w-full items-center justify-between">
+        <Link
+          href={"/"}
+          className="text-2xl max-sm:text-lg text-white font-semibold"
+        >
           TypeFaster
         </Link>
         <UserButton />
