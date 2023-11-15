f505636e7a487fa66cffa1808e60de73b2aee72b
bruno <brunoqueiroz612@gmail.com>
2023-11-15 07:57:36 -0300

    Add CredentialsProvider

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,9 +3,47 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { User, signInUser } from "@/services/api/signInUser";
+import { ServerDefaultResponse } from "@/services/api/config";
+
+interface ExtendedCredentials {
+  email: string;
+  password: string;
+  name?: string;
+  action: "sign-in" | "sign-up";
+  csrfToken: string;
+  callbackUrl: string;
+  json: boolean;
+}
 
 export const authOptions: NextAuthOptions = {
   providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const { name, email, password, action } =
+          credentials as ExtendedCredentials;
+        console.log("credentials", credentials);
+
+        let response: null | ServerDefaultResponse<User> = null;
+        if (action === "sign-up" && name) {
+          response = await createUser({ name, email, password });
+        } else if (action === "sign-in") {
+          response = await signInUser({ email, password });
+        }
+
+        if (response?.isOk) {
+          return response.data;
+        } else {
+          return null;
+        }
+      },
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
@@ -17,7 +55,9 @@ export const authOptions: NextAuthOptions = {
   ],
   callbacks: {
     async signIn({ user }) {
-      await createUser(user);
+      console.log("sign in cb", user);
+
+      // await createUser(user);
       return true;
     },
   },
