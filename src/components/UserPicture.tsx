e6312087055c64165617ed4f174d5718bf77ed5e
bruno <brunoqueiroz612@gmail.com>
2023-11-22 07:22:05 -0300

    Add useSession to get user image

--- src/components/UserPicture.tsx
+++ src/components/UserPicture.tsx
@@ -1,14 +1,15 @@
-import { getCurrentUser } from "../../lib/session";
+"use client";
+import { useSession } from "next-auth/react";
 import guestPicture from "../../public/guest.png";
 
 import Image from "next/image";
 
-const UserPicture = async () => {
-  const user = await getCurrentUser();
+const UserPicture = () => {
+  const { data } = useSession();
 
   return (
     <Image
-      src={user?.image || guestPicture}
+      src={data?.user?.image || guestPicture}
       width={45}
       height={45}
       alt="user profile picture"
