cb48262ec3c95e2bbc1bf33da12110b273aa82b0
bruno <brunoqueiroz612@gmail.com>
2023-11-23 16:32:24 -0300

    Add title and description to page

--- src/app/progress/page.tsx
+++ src/app/progress/page.tsx
@@ -3,6 +3,13 @@ import Title from "@/components/Title";
 import { getProgress } from "@/services/api/getProgress";
 import { getCurrentUser } from "../../../lib/session";
 import ProgressBarChart from "@/components/ProgressBarChart";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Progress - see your progress | TypeFaster",
+  description:
+    "Keep track of your progress through charts that display statistics of your typing",
+};
 
 const Page = async () => {
   const user = await getCurrentUser();
