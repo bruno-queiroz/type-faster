0dcdec3733a5f6ab912852a9cf28fe6f6fbc7e65
bruno <brunoqueiroz612@gmail.com>
2023-11-24 07:39:08 -0300

    Add try and catch to getProgress

--- src/services/api/getProgress.ts
+++ src/services/api/getProgress.ts
@@ -1,17 +1,29 @@
 import { ServerDefaultResponse } from "./config";
 
 export interface Progress {
-  overallAverageCpm: number;
-  overallAverageTypos: number;
-  progress: {
-    createdAt: string;
-    cpm: number;
-  }[];
+  overallAverageCpm?: number;
+  overallAverageTypos?: number;
+  progress?:
+    | {
+        createdAt: string;
+        cpm: number;
+      }[];
 }
 
 export const getProgress = async (email: string) => {
-  const response = await fetch(`http://localhost:3333/api/progress/${email}`);
-  const data: ServerDefaultResponse<Progress> = await response.json();
+  try {
+    const response = await fetch(`http://localhost:3333/api/progress/${email}`);
+    const data: ServerDefaultResponse<Progress> = await response.json();
 
-  return data;
+    return data;
+  } catch (err) {
+    console.log("Error getting progress", err);
+
+    const data: ServerDefaultResponse<null> = {
+      data: null,
+      message: "Something went wrong getting your progress.",
+      isOk: false,
+    };
+    return data;
+  }
 };
