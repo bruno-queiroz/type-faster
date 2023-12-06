74f528fca4699224f1421b842e702b9cdaa95f10
bruno <brunoqueiroz612@gmail.com>
2023-12-06 07:15:05 -0300

    Remove getUserFromStack fetch function

--- src/services/api/getUserFromStack.ts
+++
@@ -1,8 +0,0 @@
-import { deployUrl } from "./config";
-import { CreateUser } from "./createUser";
-
-export const getUserFromStack = async () => {
-  const response = await fetch(`${deployUrl}/api/user`);
-  const data: CreateUser | undefined = await response.json();
-  return data;
-};
