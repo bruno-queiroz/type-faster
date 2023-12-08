4f7c11d44f45350040b7ca57fa7ca17f3562aac0
bruno <brunoqueiroz612@gmail.com>
2023-12-08 07:51:55 -0300

    Create checkInput utility function

---
+++ src/utils/checkInput.ts
@@ -0,0 +1,26 @@
+interface CheckInput {
+  selectionStart: number;
+  keyPressed: string;
+  textArray: string[];
+  currentWordBeginningIndex: number;
+}
+
+export const checkInput = ({
+  selectionStart,
+  keyPressed,
+  textArray,
+  currentWordBeginningIndex,
+}: CheckInput) => {
+  const inputIndex = currentWordBeginningIndex + selectionStart;
+  if (keyPressed === textArray[inputIndex]) {
+    return {
+      isMisspelled: false,
+      index: 0,
+    };
+  }
+
+  return {
+    isMisspelled: true,
+    index: inputIndex,
+  };
+};
